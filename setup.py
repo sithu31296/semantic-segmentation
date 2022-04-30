2aa4f498eb5ead2bb5c1276291016e02a0c1e678
sithu31296 <sithu31296@gmail.com>
2022-04-30 19:14:36 +0630

    add rich dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='semseg',
-    version='0.4.0',
+    version='0.4.1',
     description='SOTA Semantic Segmentation Models',
     url='https://github.com/sithu31296/semantic-segmentation',
     author='Sithu Aung',
@@ -17,6 +17,7 @@ setup(
         'matplotlib',
         'tensorboard',
         'fvcore',
-        'einops'
+        'einops',
+        'rich',
     ]
 )
